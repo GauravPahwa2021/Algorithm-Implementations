b7b6b8337de780181780c34d839fbda4bb93157b
Gaurav Pahwa <86942742+GauravPahwa2021@users.noreply.github.com>
2022-08-22 10:45:39 +0530

    Update FibonacciSearch_Algorithm.cpp

--- Searching Algorithms/FibonacciSearch_Algorithm.cpp
+++ Searching Algorithms/FibonacciSearch_Algorithm.cpp
@@ -15,13 +15,13 @@ int FibonacciSearch(int A[],int N,int data)
 	int offset=-1;
 	while(fib_nth>1)
 	{
-	    int i=min(offset+fib_2th,N-1);
-	    if(A[i]<data)
-	    {
-	    	fib_nth=fib_1th;
-	    	fib_1th=fib_2th;
-	    	fib_2th=fib_nth - fib_1th;
-	    	offset=i;
+	        int i=min(offset+fib_2th,N-1);
+	        if(A[i]<data)
+	        {
+	    	       fib_nth=fib_1th;
+	    	       fib_1th=fib_2th;
+	    	       fib_2th=fib_nth - fib_1th;
+	    	       offset=i;
 		}
 		else if(A[i]>data)
 		{
@@ -43,17 +43,17 @@ int FibonacciSearch(int A[],int N,int data)
 int main()
 {
 	int A[]={10,22,35,40,45,50,80,82,85,90,100,235};
-    int N = sizeof(A) / sizeof(A[0]);
-    int x = 85;
-    int index = FibonacciSearch(A,N,x);
-    cout<<"array Elements Are"<<endl;
-    for(int i=0; i<N; i++)
-    {
-    	cout<<A[i]<<" ";
+        int N = sizeof(A) / sizeof(A[0]);
+        int x = 85;
+        int index = FibonacciSearch(A,N,x);
+        cout<<"array Elements Are"<<endl;
+        for(int i=0; i<N; i++)
+        {
+    	    cout<<A[i]<<" ";
 	}
-    if(index>=0)
-        cout<<"\nFound at index "<<index;
-    else
-        cout<<"\n"<<x<<" is not present in the array";
-	return 0;
+       if(index>=0)
+            cout<<"\nFound at index "<<index;
+       else
+            cout<<"\n"<<x<<" is not present in the array";
+       return 0;
 }
