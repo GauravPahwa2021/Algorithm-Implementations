5e1685a556fb21d8810ca3b00169f5ad5841de91
Gaurav Pahwa <86942742+GauravPahwa2021@users.noreply.github.com>
2022-08-22 12:23:46 +0530

    Update SelectionSort_Algorithm.cpp
    Time Complexity ->  O(N^2)
    Space Complexity -> O(1)

--- Sorting Algorithms/SelectionSort_Algorithm.cpp
+++ Sorting Algorithms/SelectionSort_Algorithm.cpp
@@ -21,17 +21,17 @@ void SelectionSort(int A[],int N)
 int main()
 {
 	int A[] = { 12, 11, 13, 5, 6, 20, 7 };
-    int N = sizeof(A)/sizeof(A[0]);
-    cout<<"Before sorting"<<endl;
-    for(int i=0; i<N; i++)
-    {
-        cout<<A[i]<<" ";
-    }
-    SelectionSort(A,N);
-    cout<<"\nAfter sorting"<<endl;
-    for(int i=0; i<N; i++)
-    {
-        cout<<A[i]<<" ";
-    }
+        int N = sizeof(A)/sizeof(A[0]);
+        cout<<"Before sorting"<<endl;
+        for(int i=0; i<N; i++)
+        {
+           cout<<A[i]<<" ";
+        }
+        SelectionSort(A,N);
+        cout<<"\nAfter sorting"<<endl;
+        for(int i=0; i<N; i++)
+        {
+           cout<<A[i]<<" ";
+        }
 	return 0;
 }
