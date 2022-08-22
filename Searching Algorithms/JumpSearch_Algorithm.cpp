33a2a72190f03fdb00f17a7703504f45b55c45a9
Gaurav Pahwa <86942742+GauravPahwa2021@users.noreply.github.com>
2022-08-22 11:11:02 +0530

    Update JumpSearch_Algorithm.cpp
    Time Complexity ->  O(sqrt(N))
    Space Complexity -> O(1)

--- Searching Algorithms/JumpSearch_Algorithm.cpp
+++ Searching Algorithms/JumpSearch_Algorithm.cpp
@@ -31,14 +31,14 @@ int JumpSearch(int A[],int N,int data)
 int main()
 {
 	int A[]={0,1,1,2,3,5,8,13,21,34,55,89,144,233,377,610};
-    int N = sizeof(A)/sizeof(A[0]);
-    int x=55;
-    cout<<"Array Elements Are"<<endl;
-    for(int i=0; i<N; i++)
-    {
-    	cout<<A[i]<<" ";
+        int N = sizeof(A)/sizeof(A[0]);
+        int x=55;
+        cout<<"Array Elements Are"<<endl;
+        for(int i=0; i<N; i++)
+        {
+    	   cout<<A[i]<<" ";
 	}
-    int index=JumpSearch(A,N,x);
-    cout<<"\nNumber "<<x<<" is at index "<<index;
+        int index=JumpSearch(A,N,x);
+        cout<<"\nNumber "<<x<<" is at index "<<index;
 	return 0;
 }
