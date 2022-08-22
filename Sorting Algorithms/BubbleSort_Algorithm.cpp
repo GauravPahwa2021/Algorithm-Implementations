721069d07d29028336860abdeb887ab9aa932956
Gaurav Pahwa <86942742+GauravPahwa2021@users.noreply.github.com>
2022-08-22 12:26:39 +0530

    Update BubbleSort_Algorithm.cpp
    Time Complexity ->  O(N^2)
    Space Complexity -> O(1)

--- Sorting Algorithms/BubbleSort_Algorithm.cpp
+++ Sorting Algorithms/BubbleSort_Algorithm.cpp
@@ -18,25 +18,25 @@ void BubbleSort(int A[],int N)
 			}
 		}
 		if(swapped==false)
-    	{
+    	        {
 		    break;
-	    }
+	        }
 	}
 }
 int main()
-{
-    int A[] = { 12, 11, 13, 5, 6, 20, 7 };
-    int N = sizeof(A)/sizeof(A[0]);
-    cout<<"Before sorting"<<endl;
-    for(int i=0; i<N; i++)
-    {
-        cout<<A[i]<<" ";
-    }
-    BubbleSort(A,N);
-    cout<<"\nAfter sorting"<<endl;
-    for(int i=0; i<N; i++)
-    {
-        cout<<A[i]<<" ";
-    }
-	return 0;	
+{ 
+       int A[] = { 12, 11, 13, 5, 6, 20, 7 };
+       int N = sizeof(A)/sizeof(A[0]);
+       cout<<"Before sorting"<<endl;
+       for(int i=0; i<N; i++)
+       {
+           cout<<A[i]<<" ";
+       }
+       BubbleSort(A,N);
+       cout<<"\nAfter sorting"<<endl;
+       for(int i=0; i<N; i++)
+       {
+           cout<<A[i]<<" ";
+       }
+       return 0;	
 } 
