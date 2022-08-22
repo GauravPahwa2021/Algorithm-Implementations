0533f34dac03562c721e6ead86b998690a6e6516
Gaurav Pahwa <86942742+GauravPahwa2021@users.noreply.github.com>
2022-08-22 12:31:51 +0530

    Update ShellSort_Algorithm.cpp
    Time Complexity -> O(Nlog(N))
    Space Complexity -> O(1)

--- Sorting Algorithms/ShellSort_Algorithm.cpp
+++ Sorting Algorithms/ShellSort_Algorithm.cpp
@@ -7,36 +7,36 @@ void ShellSort(int A[],int N)
 	{
 	    for(int j=gap; j<N; j++)
 	    {
-	    	for(int i=j-gap; i>=0; i-gap)
+	    	for(int i=j-gap; i>=0; i=i-gap)
 	    	{
 	    		if(A[i+gap]>A[i])
 	    		{
-	    			break;
-				}
-				else
-				{
-					int temp=A[i+gap];
-					A[i+gap]=A[i];
-					A[i]=temp;
-				}
+	    		        break;
+			}
+			else
+			{
+				int temp=A[i+gap];
+				A[i+gap]=A[i];
+				A[i]=temp;
 			}
 		}
+	    }
 	}
 }
 int main()
 {
-	int A[] = { 12, 11, 13, 5, 6, 20, 7 };
-    int N = sizeof(A)/sizeof(A[0]);
-    cout<<"Before sorting"<<endl;
-    for(int i=0; i<N; i++)
-    {
-        cout<<A[i]<<" ";
-    }
-    ShellSort(A,N);
-    cout<<"\nAfter sorting"<<endl;
-    for(int i=0; i<N; i++)
-    {
-        cout<<A[i]<<" ";
-    }
-	return 0;
+       int A[] = { 12, 11, 13, 5, 6, 20, 7 };
+       int N = sizeof(A)/sizeof(A[0]);
+       cout<<"Before sorting"<<endl;
+       for(int i=0; i<N; i++)
+       {
+	    cout<<A[i]<<" ";
+       }
+       ShellSort(A,N);
+       cout<<"\nAfter sorting"<<endl;
+       for(int i=0; i<N; i++)
+       {
+            cout<<A[i]<<" ";
+       }
+        return 0;
 }
