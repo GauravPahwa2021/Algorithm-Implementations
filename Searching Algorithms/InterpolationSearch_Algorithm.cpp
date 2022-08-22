9779b058a7fc694d0f4692541d3cf23751bf425c
Gaurav Pahwa <86942742+GauravPahwa2021@users.noreply.github.com>
2022-08-22 10:56:07 +0530

    Update InterpolationSearch_Algorithm.cpp
    Time Complexity ->  O(log(log(N))
    Space Complexity -> O(1)

--- Searching Algorithms/InterpolationSearch_Algorithm.cpp
+++ Searching Algorithms/InterpolationSearch_Algorithm.cpp
@@ -6,17 +6,17 @@ int InterpolationSearch(int A[],int N,int data)
 	int low=0,high=N-1;
 	while(low<=high && data>=A[low] && data<=A[high])
 	{
-        if(low==high)
-        {
-            if(A[low]==data)
-            {
-            	return low;
-			}
-			else
-			{
-				return -1;
-			}
-        }
+               if(low==high)
+               {
+                    if(A[low]==data)
+                    {
+            	        return low;
+		    }
+		    else
+		    {
+		        return -1;
+		    }
+                }
 		int mid=low+((high-low)/(A[high]-A[low]))*(data-A[low]);
 		if(data==A[mid])
 		{
@@ -35,16 +35,16 @@ int InterpolationSearch(int A[],int N,int data)
 }
 int main()
 {
-    int A[]={5,10,15,20,25,30,35,40,45,50};
-    int N=sizeof(A)/sizeof(A[0]);
-    cout<<"Array Elements"<<endl;
-    for(int i=0; i<N; i++)
-    {
-        cout<<A[i]<<" ";
-    }
-    int x=InterpolationSearch(A,N,30);
-    cout<<"\nIndex value of 30 is "<<x;
-    int y=InterpolationSearch(A,N,20);
-    cout<<"\nIndex value of 20 is "<<y;
-	return 0;
+       int A[]={5,10,15,20,25,30,35,40,45,50};
+       int N=sizeof(A)/sizeof(A[0]);
+       cout<<"Array Elements"<<endl;
+       for(int i=0; i<N; i++)
+       {
+           cout<<A[i]<<" ";
+       }
+       int x=InterpolationSearch(A,N,30);
+       cout<<"\nIndex value of 30 is "<<x;
+       int y=InterpolationSearch(A,N,20);
+       cout<<"\nIndex value of 20 is "<<y;
+       return 0;
 }
