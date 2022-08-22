8f140554443550bc838d87b2be2a5083e8fccb8e
Gaurav Pahwa <86942742+GauravPahwa2021@users.noreply.github.com>
2022-08-22 11:04:48 +0530

    Update LinearSearch_Algorithm.cpp
    Time Complexity ->  O(N)
    Space Complexity -> O(1)

--- Searching Algorithms/LinearSearch_Algorithm.cpp
+++ Searching Algorithms/LinearSearch_Algorithm.cpp
@@ -34,32 +34,32 @@ int ordered_LinearSearch(int A[],int N,int data)
 int main()
 {
 	int A[]={26,71,56,91,37,49,81,61,15};
-    int B[]={5,10,15,20,25,30,35,40,45,50};
-    int N1=sizeof(A)/sizeof(A[0]);
-    int N2=sizeof(B)/sizeof(B[0]);
-    cout<<"Array A Elements"<<endl;
-    for(int i=0; i<N1; i++)
-    {
-        cout<<A[i]<<" ";
-    }
-    cout<<"\nArray B Elements"<<endl;
-    for(int i=0; i<N2; i++)
-    {
-        cout<<B[i]<<" ";
-    }
-    int x=unordered_LinearSearch(A,N1,91);
-    if(x!=-1)
-    {
-    	cout<<"\n91 found in the Array A  at index "<<x;
+        int B[]={5,10,15,20,25,30,35,40,45,50};
+        int N1=sizeof(A)/sizeof(A[0]);
+        int N2=sizeof(B)/sizeof(B[0]);
+        cout<<"Array A Elements"<<endl;
+        for(int i=0; i<N1; i++)
+        {
+            cout<<A[i]<<" ";
+	}
+        cout<<"\nArray B Elements"<<endl;
+        for(int i=0; i<N2; i++)
+        {
+            cout<<B[i]<<" ";
+        }
+        int x=unordered_LinearSearch(A,N1,91);
+        if(x!=-1)
+        {
+    	        cout<<"\n91 found in the Array A  at index "<<x;
 	}
 	else
 	{
 		cout<<"\n91 did not found in the Array A";
 	}
-    int y=ordered_LinearSearch(B,N2,28);
-    if(y!=-1)
-    {
-    	cout<<"\n28 found in the Array B at index "<<y;
+        int y=ordered_LinearSearch(B,N2,28);
+        if(y!=-1)
+        {
+    	        cout<<"\n28 found in the Array B at index "<<y;
 	}
 	else
 	{
